1cbd7fbc68fa6050d580ce57b970485cce1ae1c3
Jersson Giomar Arrivasplata Rojas <jgrojas@stefanini.com>
2024-01-07 00:49:28 -0500

    chore: Proyect init

---
+++ src/main/java/com/jersson/arrivasplata/swtvap/api/campaing/CampaingApplication.java
@@ -0,0 +1,13 @@
+package com.jersson.arrivasplata.swtvap.api.campaing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampaingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampaingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jersson/arrivasplata/swtvap/api/campaing/CampaingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jersson.arrivasplata.swtvap.api.campaing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampaingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
